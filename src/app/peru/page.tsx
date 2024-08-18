1d212b800fbf834c205c056066a485d18d8fdcb3
MhalacElRetorno <matiasmatute100@gmail.com>
2024-08-17 23:32:09 -0300

    coringus

--- src/app/peru/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PaginaPrincipal() {
-  return (
-    <div className="w-[100vw] h-[100vh] flex justify-center bg-red-700">
-      <label htmlFor="">HATE PERUVIANS</label>
-      <div className="bg-black w-28 h-32 justify-center"></div>
-    </div>
-  );
-}
