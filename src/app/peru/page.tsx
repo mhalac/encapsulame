997c2071d69c05f6d77a95afcf45ca5f2bfebbfd
MhalacElRetorno <matiasmatute100@gmail.com>
2024-08-17 23:20:11 -0300

    asfd

---
+++ src/app/peru/page.tsx
@@ -0,0 +1,8 @@
+export default function PaginaPrincipal() {
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center bg-red-700">
+      <label htmlFor="">HATE PERUVIANS</label>
+      <div className="bg-black w-28 h-32 justify-center"></div>
+    </div>
+  );
+}
